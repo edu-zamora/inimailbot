58a376d5b97771eb671f80c2285e0f5da34fb0eb
Kostas Spyropoulos <inigo.aldana@gmail.com>
2010-08-30 15:16:58 -0400

    Adding GPL licence

--- templatetags/basic_math.py
+++ templatetags/basic_math.py
@@ -1,3 +1,20 @@
+# ###
+# Copyright (c) 2010 Konstantinos Spyropoulos <inigo.aldana@gmail.com>
+#
+# This file is part of inimailbot
+#
+# inimailbot is free software: you can redistribute it and/or modify it under the terms of the
+# GNU General Public License as published by the Free Software Foundation, either version 3 of
+# the License, or (at your option) any later version.
+#
+# inimailbot is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+# without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with inimailbot.
+# If not, see http://www.gnu.org/licenses/.
+# #####
+
 from google.appengine.ext import webapp
 register = webapp.template.create_template_register()
 
